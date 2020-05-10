6af05af19d222a4ffb69c265b81f400ba9fde971
Jayajit <jayajit1989@gmail.com>
2020-05-10 19:02:43 +0530

    updated_Room

--- app/src/main/java/com/dcc/covidtracker/networking/WebService.kt
+++ app/src/main/java/com/dcc/covidtracker/networking/WebService.kt
@@ -4,7 +4,7 @@ object WebService {
     /**Protocol Type**/
     private const val protocolTypeLive: String = "https://"
 
-    const val LiveUrl = "${protocolTypeLive}e096bm03f4.execute-api.ap-south-1.amazonaws.com/dev/dialogflow/"
+    const val LiveUrl = "${protocolTypeLive}api.covid19india.org/"
 
     /*call-url*/
     const val speechText: String = "getreply"
